db1379df252e79e786ae81b248b998a06d111c9e
ThmsRchrdsn <115042486+ThmsRchrdsn@users.nereply.github.com>
2023-01-22 20:23:39 +0000

    Added module descriptions

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const pbar = document.getElementById("pbar");
 
-document.addEventListener('scroll', (ev) => {
-    const th = document.body.scrollHeight - window.innerHeight;
-    const p = (window.scrollY/th) * document.body.clientWidth;
+document.addEventListener("scroll", (ev) => {
+  scrollBar();
+});
 
-    pbar.style.width = p.toString() + "px";
-})
+function scrollBar() {
+  const th = document.body.scrollHeight - window.innerHeight;
+  const p = (window.scrollY / th) * document.body.clientWidth;
+  pbar.style.width = p.toString() + "px";
+}
 
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  scrollBar();
+}
 
 // create references to the modal...
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 // to all images -- note I'm using a class!
-var images = document.getElementsByClassName('myImages');
+var images = document.getElementsByClassName("myImages");
 // the image in the modal
 var modalImg = document.getElementById("img01");
 // and the caption in the modal
@@ -34,15 +38,15 @@ var captionText = document.getElementById("caption");
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
   // and attach our click listener for this image.
-  img.onclick = function(evt) {
+  img.onclick = function (evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-  }
+  };
 }
 
 var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
